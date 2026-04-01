43ca47d2275cce14a17670a581855ac628da17cf
tarminjapan <t_amemiya_0124@outlook.jp>
2026-04-01 12:00:52 +0900

    chore(video_compressor): 🔧 __init__.pyに__all__を追加して公開APIを明確化

--- video_compressor/__init__.py
+++ video_compressor/__init__.py
@@ -4,6 +4,8 @@ Video/Audio compression script package initialization.
 
 __version__ = "1.0.6"
 
+__all__ = ["compress_audio", "compress_video", "main"]
+
 from .audio import compress_audio
 from .cli import main
 from .video import compress_video
